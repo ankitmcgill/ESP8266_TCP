9352fd7e07f4f88ef1adb08b46d479ce5a55b357
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-05-14 23:36:42 +0530

    commit with more structure to the library

--- ESP8266_TCP.c
+++ ESP8266_TCP.c
@@ -8,3 +8,162 @@
 * ***********************************************/
 
 #include "ESP8266_TCP.h"
+
+//LOCAL LIBRARY VARIABLES/////////////////////////////////////
+//TCP RELATED
+static struct espconn _esp8266_tcp_espconn;
+static struct _esp_tcp _esp8266_tcp_user_tcp;
+
+//IP / HOSTNAME RELATED
+static const char* _esp8266_tcp_host_name;
+static ip_addr_t _esp8266_tcp_host_ip;
+static ip_addr_t _esp8266_tcp_resolved_host_ip;
+static const char* _esp8266_tcp_host_path;
+static uint16_t _esp8266_tcp_host_port;
+
+//TIMER RELATED
+static uint32_t _esp8266_tcp_timer_interval;
+static os_timer_t _esp8266_tcp_dns_timer;
+static volatile os_timer_t _esp8266_tcp_timer;
+
+//TCP OBJECT STATE
+static char* _esp8266_tcp_get_request_buffer;
+static enum ESP8266_TCP_STATE _esp8266_tcp_state;
+//END LOCAL LIBRARY VARIABLES/////////////////////////////////
+
+void ICACHE_FLASH_ATTR ESP8266_TCP_Initialize(char* hostname,
+													ip_addr_t host_ip,
+													uint16_t host_port,
+													char* host_path,
+													uint32_t tcp_connection_interval_ms)
+{
+	//INITIALIZE TCP CONNECTION PARAMETERS
+	//HOSTNAME (RESOLVED THROUGH DNS IF HOST IP = NULL)
+	//HOST IP
+	//HOST PORT
+	//HOST PATH
+	//TCP CONNECTION INTERVAL MS
+
+	_esp8266_tcp_host_name = hostname;
+	_esp8266_tcp_host_ip = host_ip;
+	_esp8266_tcp_host_port = host_port;
+	_esp8266_tcp_host_path = host_path;
+	_esp8266_tcp_timer_interval = tcp_connection_interval_ms;
+}
+
+void ICACHE_FLASH_ATTR ESP8266_TCP_Intialize_Request_Buffer(uint32_t buffer_size)
+{
+	//ALLOCATE THE ESP8266 TCP GET REQUEST BUFFER
+
+	_esp8266_tcp_get_request_buffer = (char*)os_zalloc(buffer_size);
+
+	//GENERATE THE GET STRING USING HOST-NAME & HOST-PATH
+	os_sprintf(_esp8266_tcp_get_request_buffer, ESP8266_TCP_GET_REQUEST_STRING,
+			_esp8266_tcp_host_name, _esp8266_tcp_host_path);
+}
+
+void ICACHE_FLASH_ATTR ESP8266_TCP_SetDnsServer(char num_dns, ip_addr_t* dns)
+{
+	//SET DNS SERVER RESOLVE HOSTNAME TO IP ADDRESS
+	//MAX OF 2 DNS SERVER SUPPORTED (num_dns)
+
+	if(num_dns == 1 || num_dns == 2)
+	{
+		espconn_dns_setserver(num_dns, dns);
+	}
+	return;
+}
+
+void ICACHE_FLASH_ATTR ESP8266_TCP_SetCallbackFunctions()
+{
+	//HOOK FOR THE USER TO PROVIDE CALLBACK FUNCTIONS FOR
+	//VARIOUS INTERNAL TCP OPERATION
+}
+
+uint32_t ICACHE_FLASH_ATTR ESP8266_TCP_GetDataAcquisitionInterval(void)
+{
+	//RETURN ESP8266 TCP TIMER DATA ACQUISITION INTERVAL (MS)
+
+	return _esp8266_tcp_timer_interval;
+}
+
+const char* ICACHE_FLASH_ATTR ESP8266_TCP_GetSourceHost(void)
+{
+	//RETURN HOST NAME STRING
+
+	return _esp8266_tcp_host_name;
+}
+
+const char* ICACHE_FLASH_ATTR ESP8266_TCP_GetSourcePath(void)
+{
+	//RETURN HOST PATH STRING
+
+	return _esp8266_tcp_host_path;
+}
+
+void ICACHE_FLASH_ATTR  ESP8266_TCP_ResolveHostName(void* user_dns_cb_fn)
+{
+	//RESOLVE PROVIDED HOSTNAME USING THE SUPPLIED DNS SERVER
+	//AND CALL THE USER PROVIDED DNS DONE CB FUNCTION WHEN DONE
+
+	//SET USER DNS RESOLVE CB FUNCTION
+
+	//START THE DNS REOLVING PROCESS AND TIMER
+	struct espconn pespconn;
+	espconn_gethostbyname(&pespconn, _esp8266_tcp_host_name, &_esp8266_tcp_resolved_host_ip, _esp8266_tcp_dns_found_cb);
+	os_timer_arm(&_esp8266_tcp_dns_timer, 1000, 0);
+}
+
+uint16_t ICACHE_FLASH_ATTR ESP8266_TCP_GetSourcePort(void)
+{
+	//RETURN HOST REMOTE PORT NUMBER
+
+	return _esp8266_tcp_host_port;
+}
+
+void ICACHE_FLASH_ATTR ESP8266_TCP_StartDataAcqusition(void)
+{
+	//START TCP DATA ACUISITION CYCLE
+}
+
+void ICACHE_FLASH_ATTR ESP8266_TCP_StopDataAcquisition(void)
+{
+	//STOP TCP DATA ACUISITION CYCLE
+}
+
+void _esp8266_tcp_dns_timer_cb(void* arg)
+{
+	//ESP8266 DNS CHECK TIMER CALLBACK FUNCTIONS
+	//TIME PERIOD = 1 SEC
+
+	//DNS TIMER CB CALLED IE. DNS RESOLUTION DID NOT WORK
+	//DO ANOTHER DNS CALL AND REARM THE TIMER
+	struct espconn *pespconn = arg;
+	espconn_gethostbyname(pespconn, _esp8266_tcp_host_name, &_esp8266_tcp_resolved_host_ip, _esp8266_tcp_dns_found_cb);
+	os_timer_arm(&_esp8266_tcp_dns_timer, 1000, 0);
+}
+
+void _esp8266_tcp_dns_found_cb(const char* name, ip_addr_t* ipAddr, void* arg)
+{
+	//ESP8266 TCP DNS RESOLVING DONE CALLBACK FUNCTION
+
+	//DISABLE THE DNS TIMER
+	os_timer_disarm(&_esp8266_tcp_dns_timer);
+
+	if(ipAddr == NULL)
+	{
+		//HOST NAME COULD NOT BE RESOLVED
+		os_printf("hostname : %s, could not be resolved\n", _esp8266_tcp_host_name);
+
+		//CALL USER PROVIDED DNS CB FUNCTION
+		return;
+	}
+
+	//DNS GOT IP
+	os_printf("hostname : %s, resolved. IP = %d.%d.%d.%d\n", *((uint8_t*)_esp8266_tcp_resolved_host_ip.addr),
+																*((uint8_t*)_esp8266_tcp_resolved_host_ip.addr + 1),
+																*((uint8_t*)_esp8266_tcp_resolved_host_ip.addr + 2),
+																*((uint8_t*)_esp8266_tcp_resolved_host_ip.addr + 3));
+
+	//CALL USER PROVIDED DNS CB FUNCTION
+}
--- ESP8266_TCP.h
+++ ESP8266_TCP.h
@@ -1,11 +1,16 @@
-/*************************************************
+/****************************************************************
 * ESP8266 TCP LIBRARY
 *
 * MAY 11 2017
 *
 * ANKIT BHATNAGAR
 * ANKIT.BHATNAGARINDIA@GMAIL.COM
-* ***********************************************/
+*
+* REFERENCES
+* ------------
+* 	(1) https://espressif.com/en/support/explore/sample-codes
+* 	(2)
+****************************************************************/
 
 #ifndef _ESP8266_TCP_H_
 #define _ESP8266_TCP_H_
@@ -17,20 +22,48 @@
 #include "espconn.h"
 #include "os_type.h"
 
+#define ESP8266_TCP_GET_REQUEST_STRING "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n"
+
+//CUSTOM VARIABLE STRUCTURES/////////////////////////////
+enum
+{
+	ESP8266_TCP_STATE_DNS_RESOLVED,
+	ESP8266_TCP_STATE_REQUEST_SENT,
+	ESP8266_TCP_STATE_REPLY_RECEIVED,
+	ESP8266_TCP_STATE_SLEEPING,
+	ESP8266_TCP_STATE_ERROR
+} ESP8266_TCP_STATE;
+//END CUSTOM VARIABLE STRUCTURES/////////////////////////
+
+//FUNCTION PROTOTYPES/////////////////////////////////////
 //CONFIGURATION FUNCTIONS
-void ESP8266_TCP_Initialze();
-void ESP8266_TCP_SetDataReadyCb();
-void ESP8266_TCP_SetDnsServer1();
-void ESP8266_TCP_SetDnsServer2();
+void ICACHE_FLASH_ATTR ESP8266_TCP_Initialize(char* hostname,
+													ip_addr_t host_ip,
+													uint16_t host_port,
+													char* host_path,
+													uint32_t tcp_connection_interval_ms);
+void ICACHE_FLASH_ATTR ESP8266_TCP_Intialize_Request_Buffer(uint32_t buffer_size);
+void ICACHE_FLASH_ATTR ESP8266_TCP_SetDnsServer(char num_dns, ip_addr_t* dns);
+void ICACHE_FLASH_ATTR ESP8266_TCP_SetCallbackFunctions();
 
 //GET PARAMETERS FUNCTIONS
-uint32_t ESP8266_TCP_GetDataAcquisitionInterval();
-char* ESP8266_TCP_GetDataSourceHost();
-char* ESP8266_TCP_GetDataSourcePath();
-uint16_t ESP8266_TCP_GetDataSourcePort();
+uint32_t ICACHE_FLASH_ATTR ESP8266_TCP_GetDataAcquisitionInterval(void);
+const char* ICACHE_FLASH_ATTR ESP8266_TCP_GetSourceHost(void);
+const char* ICACHE_FLASH_ATTR ESP8266_TCP_GetSourcePath(void);
+uint16_t ICACHE_FLASH_ATTR ESP8266_TCP_GetSourcePort(void);
 
 //CONTROL FUNCTIONS
-void ESP8266_TCP_StartDataAcqusition();
-void ESP8266_TCP_StopDataAcquisition();
+void ICACHE_FLASH_ATTR ESP8266_TCP_ResolveHostName(void);
+void ICACHE_FLASH_ATTR ESP8266_TCP_StartDataAcqusition(void);
+void ICACHE_FLASH_ATTR ESP8266_TCP_StopDataAcquisition(void);
 
+//INTERNAL CALLBACK FUNCTIONS
+void _esp8266_tcp_dns_timer_cb(void* arg);
+void _esp8266_tcp_dns_found_cb(const char* name, ip_addr_t* ipAddr, void* arg);
+void _esp8266_tcp_connect_cb();
+void _esp8266_tcp_disconnect_cb();
+void _esp8266_tcp_send_cb();
+void _esp8266_tcp_receive_cb();
+
+//END FUNCTION PROTOTYPES/////////////////////////////////
 #endif
